a9d4d9b84def0acd18f31747c39a448459c87d53
Kishore2161 <58047720+Kishore2161@users.noreply.github.com>
2021-08-13 09:20:32 +0530

    Update Bring the consonants at top.c

--- Bring the consonants at top.c
+++ Bring the consonants at top.c
@@ -18,4 +18,55 @@ g x c N
 R m S b
 U I L L
 a a O M
+Explanation:
+The consonants in the matrix are highlighted below. 
+U I c N
+a x S b 
+g a O L 
+R m L M 
+After bringing the consonants at the top of each column, the matrix is 
+g x c N 
+R m S b 
+U I L L 
+a a O M
 */
+#include<stdio.h>
+#include<stdlib.h>
+
+int isvowel(char ch){
+    ch=tolower(ch);
+    return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u';
+}
+int main()
+{
+    int n;
+    scanf("%d\n",&n);
+    char m[n][n],ans[n][n];
+    int t[n];
+    memset(t,0,sizeof(int)*n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            scanf("%c ",&m[i][j]);
+            if (!isvowel(m[i][j])){
+                ans[t[j]][j]=m[i][j];
+                t[j]++;
+            }
+        }
+    }
+    for(int j=0;j<n;j++){
+        for(int i=0;i<n;i++){
+            if (t[j]==n)
+                break;
+            if (isvowel(m[i][j])){
+                ans[t[j]][j]=m[i][j];
+                t[j]++;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("%c ",ans[i][j]);
+        }
+        printf("\n");
+    }
+}
