f13265b91d6eaeb8a41d44424660e5d163cd78d9
Kishore2161 <58047720+Kishore2161@users.noreply.github.com>
2021-08-13 09:06:39 +0530

    Create Bring the consonants at top.c

---
+++ Bring the consonants at top.c
@@ -0,0 +1,21 @@
+/*The program must accept a character matrix of size NxN as the input. For each column in the matrix, the program must bring all the consonants at the top of the column in the same order as in the input. Finally, the program must print the modified matrix as the output.
+Boundary Condition(s):
+2 <= N <= 100
+Input Format:
+The first line contains the value of N.
+The next N lines each contain N characters separated by a space.
+Output Format:
+The first N lines each contain N characters of the modified matrix separated by a space.
+Example Input/Output 1:
+Input:
+4
+U I c N
+a x S b
+g a O L
+R m L M
+Output:
+g x c N
+R m S b
+U I L L
+a a O M
+*/
