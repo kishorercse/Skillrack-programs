c1a42b9978115ccbe5cd7bdedd658e5cdd5a0559
Kishore2161 <58047720+Kishore2161@users.noreply.github.com>
2021-08-14 09:31:48 +0530

    Create Count of common factors.c

---
+++ Count of common factors.c
@@ -0,0 +1,49 @@
+/*
+Given a set of numbers where all other numbers are multiple of smallest number, the program must find the count of the common factor C excluding 1.
+
+Example 1:
+
+Input:
+2
+100
+75
+Output:
+2
+
+Example 2:
+
+Input:
+3
+10
+20
+30
+Output:
+3
+Explanation:
+The common factors are excluding 1 are 2,5,10. Hence output is 3.
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int min=999999,n,count=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        if (arr[i]<min)
+            min=arr[i];
+    }
+    for(int f=2;f<=min;f++){
+        int t=n;
+        for(int i=0;i<n;i++){
+            if (arr[i]%f==0)
+                t-=1;
+        }
+        if (t==0)
+            count+=1;
+    }
+    printf("%d",count);
+
+}
