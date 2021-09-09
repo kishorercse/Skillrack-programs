06889ccb6625ae7b02d3ed930be820680f72196d
Kishore2161 <58047720+Kishore2161@users.noreply.github.com>
2021-09-09 20:00:02 +0530

    Update Function-printKrepeatingcharacters.c

--- Function-printKrepeatingcharacters.c
+++ Function-printKrepeatingcharacters.c
@@ -1,5 +1,6 @@
 /*
-
+The function printKrepeatingChar(char *str, int K) accepts a string str and an integer K as the input. 
+The function is supposed to print the character(s) which are consecutively repeating at least K times in the string str.
 */
 void printKrepeatingChar(char *str, int K)
 {
