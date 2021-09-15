593439cce2bb22d7dbaeb189d30a91bc31631f05
Kishore <58047720+Kishore2161@users.noreply.github.com>
2021-09-15 19:32:20 +0530

    Update last name of students.py

--- last name of students.py
+++ last name of students.py
@@ -1,3 +1,30 @@
+"""
+A String containing comma separated names of students (first name and last name) is passed as the input to the program. 
+The program must print only the last name of each student in the given order as the output. 
+
+Boundary Condition(s): 
+1 <= Length of string <= 1000 
+
+Input Format: 
+The first line contains the string. 
+
+Output Format: 
+The first line contains the last name of students separated by a space.
+
+Example Input/Output 1: 
+Input: 
+Ravi Sashtri,Mary Jane,Carl Johnson 
+
+Output:
+Sashtri Jane Johnson 
+
+Example Input/Output 2: 
+Input: 
+Johnny Depp,David Guetta,Sachin Tendulkar,Melinda Gates 
+
+Output: 
+Depp Guetta Tendulkar Gates
+"""
 s=input().strip()
 t=''
 flag=False
