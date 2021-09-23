81eff9d33d1f2a31b11f356a5b3b60bfb0cd817e
Kishore <58047720+Kishore2161@users.noreply.github.com>
2021-09-23 14:58:56 +0530

    Update prime-pair-middle-composite.py

--- prime-pair-middle-composite.py
+++ prime-pair-middle-composite.py
@@ -37,3 +37,53 @@ Input:
 Output: 
 -1
 """
+def prime(a,b):
+    f1=f2=-1
+    if a<=1:
+        f1=False
+    if b<=1:
+        f2=False
+    if a==2 or a==3:
+        f1=True
+    if a==2 or b==3:
+        f2=True
+    if f1!=-1 and f2!=-1:
+        return (f1,f2)
+    if (a!=2 and a%2==0) or (a!=3 and a%3==0):
+        f1=False
+    if (b!=2 and b%2==0) or (b!=3 and b%3==0):
+        f2=False
+    if f1!=-1 and f2!=-1:
+        return (f1,f2)
+    i=5
+    while i*i<=b:
+        if a%i==0 or a%(i+2)==0:
+            f1=False
+        if b%i==0 or b%(i+2)==0:
+            f2=False
+        if f1!=-1 and f2!=-1:
+            break
+        i+=6
+    if f1==-1:
+        f1=True
+    if f2==-1:
+        f2=True
+    return (f1,f2)
+n=int(input())
+if (prime(n,n)==(True,True)):
+    print(-1)
+else:
+    x=n-1-n%2
+    y=n+1+n%2
+    while x>1:
+        a,b=prime(x,y)
+        if a and b:
+            print(x,y)
+            break
+        elif a or b:
+            print(-1)
+            break
+        x-=2
+        y+=2
+    else:
+        print(-1)
