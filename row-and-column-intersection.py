e9a807dfbb0731319ba99844429d4d690992f38c
Kishore <58047720+Kishore2161@users.noreply.github.com>
2021-09-27 19:58:50 +0530

    Create row-and-column-intersection.py

---
+++ row-and-column-intersection.py
@@ -0,0 +1,57 @@
+"""
+Output Format: 
+The first line contains the value of X or the first line contains -1. 
+
+Example Input/Output 1: 
+Input: 
+5 
+0 1 1 1 0 
+1 0 1 0 0 
+0 0 1 0 0 
+1 0 1 0 1 
+0 1 1 0 0 
+
+Output:
+3 
+
+Explanation: 
+In the given 5*5 matrix, 3rd row contains 0s and 3rd column contains 1s, except the cell (3, 3). 
+So 3 is printed as the output. 
+
+Example Input/Output 2: 
+Input: 
+4 
+1 1 0 0 
+0 0 0 0 
+1 1 0 1 
+0 1 1 1 
+
+Output: 
+2 
+
+Example Input/Output 3: 
+Input: 
+6 
+1 1 1 1 1 1 
+0 1 1 0 0 0 
+1 1 0 0 1 0 
+0 1 0 0 0 0 
+0 1 0 1 1 0 
+0 1 0 1 1 0 
+
+Output: 
+-1
+"""
+n=int(input())
+m=[list(map(int,input().split())) for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        if i==j:
+            continue
+        if m[i][j]==1 or m[j][i]==0:
+            break
+    else:
+        print(i+1)
+        break
+else:
+    print(-1)
