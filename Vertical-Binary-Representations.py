73ce4db5dbdc1d2fb2b9c2d4135dee1bce7c2dfd
Kishore <58047720+Kishore2161@users.noreply.github.com>
2021-10-23 22:16:18 +0530

    Create Vertical-Binary-Representations.py

---
+++ Vertical-Binary-Representations.py
@@ -0,0 +1,46 @@
+"""
+The program must accept an integer N as the input. The program must print the binary representations of the integers from N to 1 vertically as shown in the Example Input/Output 
+section. 
+
+Boundary Condition(s): 
+1 <= N <= 10^5 
+
+Input Format: 
+The first line contains N. 
+
+Output Format: 
+The lines, contain the binary representations of the integers from N to 1 vertically.
+
+Example Input/Output 1: 
+Input: 
+8 
+
+Output: 
+11111111 
+0110010 
+01010
+0 
+
+Explanation: 
+The binary representation of the integers from 1 to 8 are given below. 
+1 -> 1 
+2 -> 10 
+3 -> 11 
+4 -> 100 
+5 -> 101 
+6 -> 110 
+7 -> 111 
+8 -> 1000 
+The binary representations of the integers from 8 to 1 are printed vertically. 
+
+Example Input/Output 2:
+Input: 
+17 
+
+Output:
+11111111111111111
+0011110000110010 
+00110011001010 
+0010101010 
+10
+"""
