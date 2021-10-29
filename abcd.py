c1d1925c1db44e5a6add315c5236c22d4dfdb65c
Kishore <58047720+Kishore2161@users.noreply.github.com>
2021-10-29 18:52:06 +0530

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -1,1 +1,66 @@
-The program must accept an integer matrix of size MxM and an integer N representing the size of NxN submatrix as input. The program must print the count of NxN submatrices in the MxM matrix whose border elements are same. Boundary Condition(s): 2 <= M <= 100 2 <= N <= M 1 <= Each integer value <= 1000 Input Format: The first line contains M and N separated by a space. The next M lines contain M integers separated by space(s). Output Format: The first line contains the count of NxN submatrices in the MxM matrix whose border elements are same. Example Input/Output 1: Input: 6 3 8 8 8 9 8 3 8 3 8 3 3 3 8 8 8 3 4 3 7 3 3 3 3 3 7 3 7 3 3 3 2 3 3 3 3 3 Output: 4 Explanation: There are four 3x3 submatrices having the same border elements which are highlighted below. 8 8 8 9 8 3 8 3 8 3 3 3 8 8 8 3 4 3 7 3 3 3 3 3 7 3 7 3 3 3 2 3 3 3 3 3 Example Input/Output 2: Input: 4 2 1 1 1 1 1 2 1 1 1 2 1 1 1 1 1 1 Output: 3
+"""
+The program must accept an integer matrix of size MxM and an integer N representing the size of NxN submatrix as input. The program must print the count of NxN submatrices in the 
+MxM matrix whose border elements are same.
+
+Boundary Condition(s): 
+2 <= M <= 100 
+2 <= N <= M 
+1 <= Each integer value <= 1000 
+
+Input Format: 
+The first line contains M and N separated by a space. 
+The next M lines contain M integers separated by space(s).
+
+Output Format: 
+The first line contains the count of NxN submatrices in the MxM matrix whose border elements are same. 
+
+Example Input/Output 1: 
+Input: 
+6 3 
+8 8 8 9 8 3 
+8 3 8 3 3 3 
+8 8 8 3 4 3 
+7 3 3 3 3 3 
+7 3 7 3 3 3 
+2 3 3 3 3 3 
+
+Output:
+4 
+
+Explanation: 
+There are four 3x3 submatrices having the same border elements which are highlighted below.
+8 8 8 9 8 3 
+8 3 8 3 3 3 
+8 8 8 3 4 3 
+7 3 3 3 3 3 
+7 3 7 3 3 3 
+2 3 3 3 3 3 
+
+Example Input/Output 2: 
+Input: 
+4 2
+1 1 1 1
+1 2 1 1
+1 2 1 1
+1 1 1 1
+
+Output:
+3
+"""
+m,n=map(int,input().split())
+mat=[input().split() for _ in range(m)]
+count=0
+for i in range(m-n+1):
+    for j in range(m-n+1):
+        f=True
+        for ii in range(i,i+n):
+            for jj in range(j,j+n):
+                if ii==i or jj==j or ii==i+n-1 or jj==j+n-1:
+                    if mat[ii][jj]!=mat[i][j]:
+                        f=False
+                        break
+            if not f:
+                break
+        if f:
+            count+=1
+print(count)
