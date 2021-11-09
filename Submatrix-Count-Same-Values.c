7bfb23f7eb0c7803ddcf4850a965e8d253e3fa41
Kishore <58047720+Kishore2161@users.noreply.github.com>
2021-11-09 19:21:38 +0530

    Create Submatrix-Count-Same-Values.c

---
+++ Submatrix-Count-Same-Values.c
@@ -0,0 +1,67 @@
+/*
+The program must accept an integer matrix of size RxC as the input. The program must print the number of submatrix of size 2x2 that contains only one integer (i.e., all the four 
+integers in the 2x2 submatrix are equal) as the output. 
+
+Boundary Condition(s): 
+2 <= R, C <= 50 
+
+Input Format:
+The first line contains R and C separated by a space. 
+The next R lines each contain C integers separated by a space. 
+
+Output Format:
+The first line contains the number of submatrix as per the given condition. 
+
+Example Input/Output 1: 
+Input:
+2 5
+5 1 1 2 2
+4 1 1 2 2 
+
+Output: 
+2
+
+Explanation: 
+The integers in the first 2x2 submatrix are 5, 1, 4 and 1.
+The integers in the second 2x2 submatrix are 1, 1, 1 and 1.
+The integers in the third 2x2 submatrix are 1, 2, 1 and 2. 
+The integers in the fourth 2x2 submatrix are 2, 2, 2 and 2.
+In the 2x5 integer matrix, there are 2 submatrices where all the integers are equal in it. Hence the output is 2 
+
+Example Input/Output 2:
+Input:
+4 4
+90 56 15 20
+62 62 48 15
+62 62 10 10
+62 62 10 10
+
+Output: 
+3
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int r, c, ans=0;
+    scanf("%d %d",&r,&c);
+    int m[r][c];
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            scanf("%d",&m[i][j]);
+        }
+    }
+    for(int i=0;i<r-1;i++)
+    {
+        for(int j=0;j<c-1;j++)
+        {
+            if (m[i][j]==m[i][j+1] && m[i][j+1]==m[i+1][j] && m[i+1][j]==m[i+1][j+1])
+                ans+=1;
+        }
+    }
+    printf("%d",ans);
+
+}
