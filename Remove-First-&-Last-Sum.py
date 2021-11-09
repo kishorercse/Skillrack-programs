e10388a7b9c9f0f16fe156496dbf97563a461286
Kishore <58047720+Kishore2161@users.noreply.github.com>
2021-11-09 19:13:16 +0530

    Create Remove-First-&-Last-Sum.py

---
+++ Remove-First-&-Last-Sum.py
@@ -0,0 +1,36 @@
+"""
+The program must accept two integers X and Y as the input. The program must remove the first digit and the last digit of both X and Y. Then the program must print the sum of X 
+and Y as the output. 
+
+Boundary Condition(s): 
+100 <= X, Y <= 10^8 
+
+Input Format: 
+The first line contains X and Y separated by a space. 
+
+Output Format: 
+The first line contains the sum of X and Y. 
+
+Example Input/Output 1: 
+Input:
+9874 156
+
+Output:
+92 
+
+Explanation: 
+After removing the first digit and the last digit of 9874, the integer X becomes 87. 
+After removing the first digit and the last digit of 156, the integer Y becomes 5. 
+So the sum of 87 and 5 is 92. 
+
+Example Input/Output 2: 
+Input: 
+10105 5700
+
+Output: 
+80
+"""
+x,y=input().split()
+x=x[1:-1]
+y=y[1:-1]
+print(int(x)+int(y))
