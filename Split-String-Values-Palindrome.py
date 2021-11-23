454f1b9f21fd823100e6967d63984b6ca140f16a
Kishore <58047720+kishorercse@users.noreply.github.com>
2021-11-23 20:07:07 +0530

    Update Split-String-Values-Palindrome.py

--- Split-String-Values-Palindrome.py
+++ Split-String-Values-Palindrome.py
@@ -1,5 +1,7 @@
 """
-The program must accept two string values S1 and S2 of equal length as the input. The program must split both the string values at the same position. If the concatenation of the first part of S1 and the second part of S2 forms a palindrome or the concatenation of the first part of S2 and the second part of S1 forms a palindrome, then the program must print YES as the output. Else the program must print NO as the output.
+The program must accept two string values S1 and S2 of equal length as the input. The program must split both the string values at the same position. If the concatenation of
+the first part of S1 and the second part of S2 forms a palindrome or the concatenation of the first part of S2 and the second part of S1 forms a palindrome, then the program 
+must print YES as the output. Else the program must print NO as the output.
 
 Boundary Condition(s):
 1 <= Length of S1, S2 <= 1000
