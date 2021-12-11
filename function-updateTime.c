76a7f44eed3347845b7c1baccab096bedbf1113d
Kishore <58047720+kishorercse@users.noreply.github.com>
2021-12-11 22:15:12 +0530

    Create function-updateTime.c

---
+++ function-updateTime.c
@@ -0,0 +1,50 @@
+/*
+The function/method updateTime accepts two arguments time and str. The first argument time represents a time in 24-hour format (HH:MM:SS). The string str contains an integer X
+that ends with a letter denoting the unit of time (h or H - hours, m or M - minutes, s or S - seconds).
+
+The function/method updateTime must update the time by adding X (hours or minutes or seconds).
+
+Your task is to implement the function updateTime so that the program runs successfully.
+
+The following structure is used to represent the Time and is already defined in the default code (Do not write this definition again in your code).
+struct Time
+{
+    int hours;
+    int minutes;
+    int seconds;
+};
+
+IMPORTANT: Do not write the main() function as it is already defined.
+
+Example Input/Output 1:
+Input:
+10:45:20
+50s
+
+Output:
+10:46:10
+
+Explanation:
+Here X = 50 seconds.
+After adding 50 seconds, the time becomes 10:46:10.
+
+Example Input/Output 2:
+Input:
+15:30:55
+125M
+
+Output:
+17:35:55
+
+Explanation:
+Here X = 125 minutes.
+After adding 125 minutes, the time becomes 17:35:55.
+
+Example Input/Output 3:
+Input:
+23:50:47
+2h
+
+Output:
+01:50:47
+*/
