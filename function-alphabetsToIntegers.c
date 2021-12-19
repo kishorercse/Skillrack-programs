21ba80e58c0924ffd735e263ce0ff9b2930d5697
Kishore <58047720+kishorercse@users.noreply.github.com>
2021-12-19 10:18:47 +0530

    Update function-alphabetsToIntegers.c

--- function-alphabetsToIntegers.c
+++ function-alphabetsToIntegers.c
@@ -35,3 +35,60 @@ Input:
 Output:
 -4 -601 908
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct BoundedArray
+{
+    int SIZE;
+    int *arr;
+} boundedArray;
+boundedArray* alphabetsToIntegers(char *str)
+{
+    boundedArray *bArr=malloc(sizeof(boundedArray));
+    bArr->arr=malloc(sizeof(int)*100);
+    bArr->SIZE=0;
+    bArr->arr[0]=0;
+    int neg=0;
+    for(int i=0;str[i];i++)
+    {
+        if (str[i]=='-')
+        {
+            neg=1;
+            continue;
+        }
+        if (str[i]==' ')
+        {
+            if (neg==1)
+                bArr->arr[bArr->SIZE]*=-1;
+            bArr->SIZE++;
+            bArr->arr[bArr->SIZE]=0;
+            neg=0;
+            continue;
+        }
+        bArr->arr[bArr->SIZE]=bArr->arr[bArr->SIZE]*10+(str[i]-'a');
+    }
+    if(neg==1)
+        bArr->arr[bArr->SIZE]*=-1;
+    bArr->SIZE++;
+    return bArr;
+}
+int main()
+{
+    char str[1001];
+    scanf("%[^\n]", str);
+    boundedArray *bArr = alphabetsToIntegers(str);
+    if(bArr == NULL)
+    {
+        printf("Array is not formed\n");
+    }
+    if(bArr->SIZE <= 0)
+    {
+        printf("Invalid size for the array\n");
+    }
+    for(int index = 0; index < bArr->SIZE; index++)
+    {
+        printf("%d ", bArr->arr[index]);
+    }
+    return 0;
+}
