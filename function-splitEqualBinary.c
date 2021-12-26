0ecdcb8d7bdf85935f71a8f0397f280b9367cdc0
Kishore <58047720+kishorercse@users.noreply.github.com>
2021-12-26 22:24:18 +0530

    Create function-splitEqualBinary.c

---
+++ function-splitEqualBinary.c
@@ -0,0 +1,37 @@
+/*
+The function/method splitEqualBinary accepts an argument N representing an integer value. The number of bits in the binary representation of N is always even. The 
+function/method splitEqualBinary must split the binary representation of N into two equal halves. If the two halves are same, then the function must return the sum of decimal 
+equivalents of the two equal halves. Else the function must return the integer N as it is. Your task is to implement the function splitEqualBinary so that the program runs 
+successfully. IMPORTANT: Do not write the main() function as it is already defined. 
+
+Example Input/Output 1: 
+Input: 
+10
+
+Output:
+4
+
+Explanation:
+Here N = 10.
+10 -> 1010 -> 10 == 10.
+2 + 2 = 4. 
+
+Example Input/Output 2:
+Input:
+187 
+
+Output: 
+22
+
+Explanation:
+Here N = 187. 
+187 -> 10111011 -> 1011 == 1011.
+11 + 11 = 22.
+
+Example Input/Output 3: 
+Input: 
+8
+
+Output:
+8
+*/
