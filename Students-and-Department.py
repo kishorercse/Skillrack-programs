f3a4e2f18a518eda5d46a9c6124de6c5c84ee768
Kishore <58047720+kishorercse@users.noreply.github.com>
2022-01-04 19:00:07 +0530

    Create Students-and-Department.py

---
+++ Students-and-Department.py
@@ -0,0 +1,30 @@
+"""
+Given N students name and department, print the X students belonging to a specific department D.
+
+Boundary Condition(s):
+1 <= N <= 100
+Length of student name is from 3 to 100
+Length of department name is from 3 to 20
+
+Input Format:
+The first line contains the values of N.
+N lines contain the name and department of N students separated by a space.
+The next line (N+2)th line, will contain the department name D for which the students list is to be printed.
+
+Output Format:
+X lines containing students name where X is the number of students belonging to department D.
+
+Example Input/Output 1:
+Input:
+5
+Arun CSE
+Bhuvana ECE
+Ganesh MECH
+Pavithra ECE
+Rohit CSE
+ECE
+
+Output:
+Bhuvana
+Pavithra
+"""
