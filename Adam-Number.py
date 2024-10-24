8e4b5950c22bc9632dee0464461b6acf3f8c5d53
Kishore <58047720+kishorercse@users.noreply.github.com>
2024-10-24 08:26:59 +0530

    Update Adam-Number.py
    Added question from letuscrack

--- Adam-Number.py
+++ Adam-Number.py
@@ -1,3 +1,8 @@
+"""
+A number is said to be an Adam number if the reverse of the square of the number is equal to the square of the reverse of the number.  
+For example, 12 is an Adam number because the reverse of the square of 12 is the reverse of 144, which is 441, and the square of the reverse 
+of 12 is the square of 21, which is also 441.
+"""
 def reverse(n):
     return int(str(n)[::-1])
 n=int(input())
